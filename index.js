6a9df89ecb01e51aa62bb23bbe69a60d2aa9baa6
FerZamora8 <ferzamora@gmail.com>
2024-06-27 10:17:41 -0600

    commit definitivo

---
+++ index.js
@@ -0,0 +1,8 @@
+const mate=require('./mate.js');
+console.log("la suma es ", mate.sumar(5,5));
+
+console.log("la resta es ", mate.restar(5,5));
+
+console.log("la multiplicacion es ", mate.multiplicar(5,5));
+
+console.log("la division es ", mate.dividir(5,5));
